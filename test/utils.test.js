ae86801a5137e4ced330fc2e7537602f6b57552c
Ana-Marie <anamarie1907@gmail.com>
2021-10-16 13:57:02 -0300

    teste falhando

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,7 +6,18 @@ describe('Utils', () => {
             expect(gerarNumeroAleatorio(20, -5))
                 .toBe(-1);
         });
+        test('inicio deve ser um inteiro', () =>{
+            expect(gerarNumeroAleatorio(3.5,5))
+            .toBe(-1);
+        });
+        test('fim deve ser um inteiro',()=>{
+            expect(gerarNumeroAleatorio(20,"notInteger")).toBe(-1);
+        });
+        test('inicio não pode ser negativo',()=>{
+            expect(gerarNumeroAleatorio(-7,9)).toBe(-1);
+        });
     });
+    
 
     describe('acharCaracter', () => {
         test('comprimento invalido', () => {
